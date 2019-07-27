11399fa0b654a399de2f2722c236c13b5e1cb42c
vidklopcic <klopcic.vid@gmail.com>
2019-07-27 19:34:07 +0200

    plot of both precessions

--- analysis.py
+++ analysis.py
@@ -4,6 +4,11 @@ from math import atan2
 import matplotlib.pyplot as plt
 import numpy as np
 
+step_s = 60 * 60 * 12
+step_d = step_s / (24 * 60 * 60)
+step_m = step_s / (24 * 60 * 60 * 30)
+step_y = step_s / (24 * 60 * 60 * 365)
+
 
 class GravityPlot:
     def __init__(self):
@@ -22,17 +27,20 @@ class GravityPlot:
         ])
         return np.matmul(rmat, vect)[:, 0]
 
-    def load_data(self, dir):
+    def load_data(self, dir, n_steps=None):
         self.dir = dir
-        self.sonce_d = np.loadtxt(os.path.join(dir, 'sonce.txt'))
-        self.zemlja_d = np.loadtxt(os.path.join(dir, 'zemlja.txt'))
-        self.luna_d = np.loadtxt(os.path.join(dir, 'luna.txt'))
+        self.sonce_d = np.loadtxt(os.path.join(dir, 'sonce.txt'))[:n_steps]
+        self.zemlja_d = np.loadtxt(os.path.join(dir, 'zemlja.txt'))[:n_steps]
+        self.luna_d = np.loadtxt(os.path.join(dir, 'luna.txt'))[:n_steps]
+        # earth - centered
         ldata = []
         for i in range(len(self.luna_d)):
-            ldata.append(self.luna_d[i]-self.zemlja_d[i])
+            ldata.append(self.luna_d[i] - self.zemlja_d[i])
 
         self.luna_orbit = np.array(ldata)
         plt.title(self.dir)
+
+        # moon orbit nodes
 
     def angle_2d(self, v1, v2):
         """ Returns the angle in radians between vectors 'v1' and 'v2'    """
@@ -42,11 +50,11 @@ class GravityPlot:
 
     def plot_zemlja(self):
         z = self.zemlja_d.T
-        plt.plot(z[0], z[1], color='b', linewidth=2, label='zemlja')
+        plt.plot(z[0], z[1], color='b', linewidth=0.3, label='zemlja')
 
     def plot_luna(self):
         z = self.zemlja_d.T
-        plt.plot(z[0], z[1], color='g', linewidth=1, label='luna')
+        plt.plot(z[0], z[1], color='g', linewidth=0.3, label='luna')
 
     def plot_planet(self, color, label):
         plt.scatter([0], [0], color=color, label=label)
@@ -74,10 +82,97 @@ class GravityPlot:
         l = self.luna_orbit.T
         plt.plot(l[0], l[1], color='g', linewidth=0.3, label='luna')
 
+    def plot_luna_sonce_zemlja_angle(self):
+        data = []
+        plt.title('Kot Luna-Zemlja-Sonce')
+        for i in range(len(self.zemlja_d)):
+            z = self.zemlja_d[i][:2]
+            l = self.luna_d[i][:2]
+            d_zl = z - l
+            z_dir = -z / np.linalg.norm(z)
+            l_dir = -d_zl / np.linalg.norm(d_zl)
+            data.append(np.arccos(np.dot(z_dir, l_dir)))
+
+        plt.xlabel('t [dan]')
+        plt.ylabel('kot [rad]')
+        plt.plot(np.linspace(0, step_d * len(self.zemlja_d), num=len(data)), data, label='kot L-Z-S')
+
+    def plot_luna_zemlja_dist_in_sun_direction(self):
+        data = []
+        for i in range(len(self.zemlja_d)):
+            dir = self.zemlja_d[i] / np.linalg.norm(self.zemlja_d[i])
+            data.append(np.linalg.norm(np.dot(self.zemlja_d - self.luna_d, dir)))
+        plt.plot(np.linspace(0, step_d * len(self.zemlja_d), num=len(data)), data,
+                 label='razdalja med luno in zemljo v smeri sonca')
+
+    def plot_nodes_angle(self):
+        '''
+        Kot med zveznico presecisc lunine orbite s ploskvijo zemljine orbite in zveznico med enakonocji.
+        '''
+
+        plt.title('Kot med vozelno črto Lune in zveznico med enakonočji')
+        ref_v = np.array([0, 1])
+
+        data = []
+        ecliptic_vectors = []
+        min_z = 0
+        decreasing = False
+        invert = False
+        for i in range(len(self.zemlja_d)):
+            lz = abs(self.luna_d[i][2])
+            if lz > min_z:
+                if decreasing:
+                    v = self.luna_d[i] - self.zemlja_d[i]
+                    if invert:
+                        v *= -1
+                    invert = not invert
+                    ecliptic_vectors.append(v)
+                decreasing = False
+            else:
+                decreasing = True
+            min_z = lz
+
+        for ev in ecliptic_vectors:
+            dir_v = ev[:2] / np.linalg.norm(ev[:2])
+            data.append(np.arccos(np.dot(dir_v, ref_v)))
+
+        plt.plot(np.linspace(0, step_d * len(self.zemlja_d), num=len(data)), data, label='kot')
+
+    def plot_major_axis_angle(self):
+        '''
+        Kot med zveznico presecisc lunine orbite s ploskvijo zemljine orbite in zveznico med enakonocji.
+        '''
+
+        plt.title('Kot med glavno osjo orbite Lune in zveznico med enakonočji')
+        ref_v = np.array([1, 0])
+        data = []
+        nodes = []
+        min_d = 0
+        decreasing = False
+        increasing = True
+        for i in range(len(self.luna_d)):
+            dlz = np.linalg.norm(self.luna_d[i]-self.zemlja_d[i])
+            if dlz > min_d:
+                # if decreasing:
+                #     nodes.append(-(self.luna_d[i]-self.zemlja_d[i]))
+                decreasing = False
+                increasing = True
+            elif dlz < min_d:
+                if increasing:
+                    nodes.append(self.luna_d[i]-self.zemlja_d[i])
+                increasing = False
+                decreasing = True
+            min_d = dlz
+
+        for node in nodes:
+            dir_v = node[:2] / np.linalg.norm(node[:2])
+            data.append(np.arccos(np.dot(dir_v, ref_v)))
+
+        plt.plot(np.linspace(0, step_y * len(self.zemlja_d), num=len(data)), data, label='kot')
+
     def plot_luna_eq_orbit(self):
         l = self.luna_orbit.T
         plt.plot(l[0], l[2], color='g', linewidth=0.3, label='luna')
-
 
     def plot_all(self):
         self.g_plot_orbit()
@@ -101,8 +196,10 @@ def plot_dirs():
 gplot = GravityPlot()
 
 for dir in plot_dirs():
-    gplot.load_data(dir)
-    gplot.g_plot_luna_eq_orbit()
+    gplot.load_data(dir, n_steps=int(9/step_y))
+    # plt.gca().set_aspect('equal', adjustable='box')
+    gplot.plot_major_axis_angle()
+    plt.legend()
     plt.show()
-    # plt.savefig(dir + '/luna_4_leta_eq.pdf')
+    # plt.savefig(dir + '/kot_zemlja_sonce_luna.pdf')
     if plot == PLOT_LAST: break
--- params.py
+++ params.py
@@ -5,8 +5,8 @@ m_z = 5.9722e24
 m_l = 7.3477e22
 
 # Luna - aphelij
-d_zl_max = 405696e3
-v_zl_min = 970
+d_zl_max = 405948e3
+v_zl_min = 959.6
 angle_ekliptika = 0.08988446
 
 # Zemlja - aphelij
@@ -14,4 +14,4 @@ d_zs_max = 1.521432e11
 v_zs_min = 29290
 
 period = 20 * 365.24 * 24 * 60 * 60
-timestep = 60 * 60 * 12
+timestep = 60 * 60
